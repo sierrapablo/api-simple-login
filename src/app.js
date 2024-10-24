6ccb23fabe61c84392b5fcd1a7a9902512620aa0
Pablo Sierra Lorente <pablosierralorente@outlook.es>
2024-10-24 15:07:49 +0200

    app.js v1

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const webapp_hostname = process.env.WEBAPP_HOSTNAME;
+
+app.use(cors(
+    {
+        origin: webapp_hostname,
+        methods: ['POST'],
+        allowedHeaders: ['*']
+    }
+));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', userRoutes);
+
+app.listen(port, () => {
+    console.log(`API listening on port ${port}`);
+  });
+  
