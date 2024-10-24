93a66c4b3ac0152eccd9194badbb7818ebeea867
Pablo Sierra Lorente <pablosierralorente@outlook.es>
2024-10-24 15:43:11 +0200

    remove sql hardcoding and add try-catch blocks

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,13 +1,31 @@
 const pool = require('../config/database');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
 
+
+let queryByUsernameSQL;
+try {
+  queryByUsernameSQL = fs.readFileSync(
+    path.join(__dirname, `../queries/${process.env.BY_USERNAME_SQL}`),
+    'utf8'
+  );
+} catch (error) {
+  console.error(`Error reading SQL file: ${error.message}`);
+  throw new Error('Failed to load SQL query');
+}
 
 const findUserByUsername = async (username) => {
-  const query = 'SELECT * FROM public.user_list WHERE username = $1';
-  const result = await pool.query(query, [username]);
-  return result.rows[0] || null;
+  try {
+    const result = await pool.query(queryByUsernameSQL, [username]);
+    return result.rows[0] || null;
+  } catch (error) {
+    console.error(`Database query error: ${error.message}`);
+    throw new Error('Database query failed');
+  }
 };
 
 
 module.exports = {
-    findUserByUsername
-  };
+  findUserByUsername
+};
