eb42a51a7637fba96a21665b31fe216fbd143963
Pablo Sierra Lorente <pablosierralorente@outlook.es>
2024-10-24 15:26:29 +0200

    linting

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -8,5 +9,6 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+
 
 module.exports = pool;
